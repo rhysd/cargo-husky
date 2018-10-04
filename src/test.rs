4e4b71343b63189788a449efade6d38aff49aea2
rhysd <lin90162@yahoo.co.jp>
2018-10-04 12:46:43 +0900

    add tests where someone else already put hook script

--- src/test.rs
+++ src/test.rs
@@ -251,5 +251,60 @@ fn regenerate_hook_script_on_package_update() {
     );
 }
 
-// TODO: Test foregin hook script is already there which has less than 2 lines
-// TODO: Test foregin hook script is already there which has more than 2 lines
+#[test]
+fn another_hook_less_than_3_lines_already_exists() {
+    let root = cargo_project_for("another-hook-script-less-than-3-lines");
+    let prepush_path = hook_path(&root, "pre-push");
+    let content = "#!/bin/sh\necho 'hook put by someone else'".to_string();
+    let modified_before = {
+        let mut f = File::create(&prepush_path).unwrap();
+        writeln!(f, "{}", content);
+        f.metadata().unwrap().modified().unwrap()
+    };
+
+    // Ensure modified time differs from previous if file were updated
+    thread::sleep(time::Duration::from_secs(1));
+
+    run_cargo(&root, &["test"]);
+
+    let modified_after = File::open(&prepush_path)
+        .unwrap()
+        .metadata()
+        .unwrap()
+        .modified()
+        .unwrap();
+
+    assert_eq!(modified_before, modified_after);
+
+    let script = get_hook_script(&root, "pre-push").unwrap();
+    assert_eq!(content + "\n", script);
+}
+
+#[test]
+fn another_hook_more_than_3_lines_already_exists() {
+    let root = cargo_project_for("another-hook-script-more-than-3-lines");
+    let prepush_path = hook_path(&root, "pre-push");
+    let content = "#!/bin/sh\n\n\necho 'hook put by someone else'".to_string();
+    let modified_before = {
+        let mut f = File::create(&prepush_path).unwrap();
+        writeln!(f, "{}", content);
+        f.metadata().unwrap().modified().unwrap()
+    };
+
+    // Ensure modified time differs from previous if file were updated
+    thread::sleep(time::Duration::from_secs(1));
+
+    run_cargo(&root, &["test"]);
+
+    let modified_after = File::open(&prepush_path)
+        .unwrap()
+        .metadata()
+        .unwrap()
+        .modified()
+        .unwrap();
+
+    assert_eq!(modified_before, modified_after);
+
+    let script = get_hook_script(&root, "pre-push").unwrap();
+    assert_eq!(content + "\n", script);
+}
